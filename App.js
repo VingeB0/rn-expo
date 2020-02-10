6db4984d8a5553cce9361a904d25707f15f2e388
Vladymyr Davii <vladimir.davii@gmail.com>
2020-02-10 02:13:22 +0200

    try hooks with state, some new components like ScrollView, FlatList and TextInput params

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import { Navbar } from './src/Navbar.js';
 import { AddTodo } from './src/AddTodo.js';
+import { Todo } from './src/Todo.js';
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+    // setTodos(todos.concat([ newTodo ]));
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title
+      }
+    ])
+  };
+
   return (
+    // <ScrollView>
     <View>
       <Navbar title={'Todo app'}/>
       <View style={styles.container}>
-        <AddTodo/>
+        <AddTodo onSubmit={addTodo}/>
+
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({item}) => <Todo todo={item} /> }
+        />
+
+        {/*<View>*/}
+        {/*  {todos.map((todo) => (*/}
+        {/*    <Todo todo={todo} key={todo.id} />*/}
+        {/*  ))}*/}
+        {/*</View>*/}
       </View>
     </View>
   );
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import {View, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, TextInput, Button, Text, Alert} from 'react-native';
 
-export const AddTodo = props => {
-  const {title} = props;
+export const AddTodo = ({ onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const pressHandler = () => {
+    if(value.trim()) {
+      onSubmit(value);
+      setValue('');
+    } else {
+      Alert.alert('Name cannot be empty');
+    }
+  };
+
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input}/>
-      <Button title="Add"/>
+      <TextInput
+        style={styles.input}
+        // onChangeText={text => setValue(text)}
+        onChangeText={setValue}
+        value={value}
+        placeholder="Type some text..."
+        autoCorrect={false}
+        autoCapitalize='none'
+        keyboardType='number-pad'
+      />
+      <Button onPress={pressHandler} title="Add"/>
     </View>
   )
 };
@@ -15,7 +34,8 @@ const styles = StyleSheet.create({
   block: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 15
   },
   input: {
     padding: 10,
